9f3a70c7cf6555a07ce068fe423d3eef6c0079f9
TaliaMukami <156464644+Tash465@users.noreply.github.com>
2025-11-10 08:39:07 +0300

    Enhance ProductAdmin with image display feature
    Added image_tag method to display product images in admin.

--- products/admin.py
+++ products/admin.py
@@ -1,16 +1,17 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from .models import Product
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'category', 'price', 'unit', 'available', 'featured', 'stock_quantity', 'date_added')
-    list_filter = ('category', 'available', 'featured')  # Add featured to filters
+    list_display = ('name', 'category', 'price', 'unit', 'available', 'featured', 'stock_quantity', 'date_added', 'image_tag')
+    list_filter = ('category', 'available', 'featured')
     search_fields = ('name', 'description')
     ordering = ('category', 'name')
     
     fieldsets = (
         (None, {
-            'fields': ('name', 'category', 'description', 'price', 'unit', 'stock_quantity', 'image', 'available', 'featured')  # Add featured here
+            'fields': ('name', 'category', 'description', 'price', 'unit', 'stock_quantity', 'image', 'available', 'featured')
         }),
         ('Date Info', {
             'fields': ('date_added',),
@@ -18,3 +19,10 @@ class ProductAdmin(admin.ModelAdmin):
         }),
     )
     readonly_fields = ('date_added',)
+
+    # Display a thumbnail in the admin
+    def image_tag(self, obj):
+        if obj.image:
+            return format_html('<img src="{}" width="50" />', obj.image.url)
+        return "-"
+    image_tag.short_description = 'Image'
